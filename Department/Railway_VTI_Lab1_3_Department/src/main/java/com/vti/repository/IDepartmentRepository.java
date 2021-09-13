3ceab970f43ae06e6c0f8d5e40e60993ffd0b554
xmen1xb <81312038+xmen1xb@users.noreply.github.com>
2021-09-14 03:43:19 +0700

    Delete Department directory

--- Department/Railway_VTI_Lab1_3_Department/src/main/java/com/vti/repository/IDepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vti.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.vti.entity.Department;
-
-public interface IDepartmentRepository extends JpaRepository<Department, Short> {
-
-}
